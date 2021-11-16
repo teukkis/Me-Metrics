e097ed3fcef83b8ae794e6ff82969fc7695f1ec0
teukkis <teemu.varsala@gmail.com>
2021-11-16 23:20:31 +0200

    reorganized

---
+++ express_server/server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+var port = 3100;
+
+//hello world. currently fetched from home.js in client
+app.get("/placeholder", (req, res) => {
+    res.json({message: "Hello world!"});
+})
+
+
+
+app.listen(port, () =>{
+    console.log('Server listening at '+port);
+});
